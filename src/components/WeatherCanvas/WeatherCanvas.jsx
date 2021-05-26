95362f1fc3104f56ee9efa2f426b6ba77c477fde
andy <andersgee@gmail.com>
2021-05-26 16:19:05 +0200

    useWeather depends on lon lat (important: dont use arrays for useEffect dependecies.)

--- src/components/WeatherCanvas/WeatherCanvas.jsx
+++ src/components/WeatherCanvas/WeatherCanvas.jsx
@@ -3,8 +3,9 @@ import * as styles from "./weathercanvas.module.scss";
 import useWeather from "@hooks/useWeather";
 
 export default function WeatherCanvas() {
-  const w = useWeather();
-
+  const lon = 16.33;
+  const lat = 59.91;
+  const w = useWeather(lon, lat);
   console.log(w);
 
   return (
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-async function fetchweather(lon = 16.33, lat = 59.91) {
+async function fetchweather(lon, lat) {
   const appid = "e95b492d3c977db6b58b95f097683036";
   const baseurl = "https://api.openweathermap.org/data/2.5/forecast";
   const url = `${baseurl}?lon=${lon}&lat=${lat}&appid=${appid}`;
@@ -13,16 +13,16 @@ async function fetchweather(lon = 16.33, lat = 59.91) {
   return forecast;
 }
 
-export default function useWeather() {
+export default function useWeather(lon, lat) {
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    fetchweather().then((w) => {
-      if (w?.cod == "200") {
+    fetchweather(lon, lat).then((w) => {
+      if (w?.cod === "200") {
         setWeather(w);
       }
     });
-  }, []);
+  }, [lon, lat]);
 
   return weather;
 }
