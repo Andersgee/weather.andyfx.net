fabef3a35221b69c8ac6822e5ce139be83c46af6
andy <andersgee@gmail.com>
2021-05-27 15:45:32 +0200

    nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,24 @@
 import * as React from "react";
 import * as styles from "./nav.module.scss";
+import clsx from "clsx";
 
 export default function Nav() {
-  return <div className={styles.nav}>Nav</div>;
+  const [active, setActive] = React.useState(0);
+
+  return (
+    <div className={styles.nav}>
+      <button
+        onClick={() => setActive(0)}
+        className={clsx(styles.navbutton, active === 0 && styles.active)}
+      >
+        Table
+      </button>
+      <button
+        onClick={() => setActive(1)}
+        className={clsx(styles.navbutton, active === 1 && styles.active)}
+      >
+        Visual
+      </button>
+    </div>
+  );
 }
