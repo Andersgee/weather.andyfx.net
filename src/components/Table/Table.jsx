c016ab08af9765ee9226211109ecc4ca998546bd
andy <andersgee@gmail.com>
2021-05-27 16:29:51 +0200

    monospace font for table

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,12 +1,62 @@
 import * as React from "react";
 import * as styles from "./table.module.scss";
 
-export default function Table() {
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "Maj",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Okt",
+  "Nov",
+  "Dec",
+];
+
+function militarytimeFromHour(h) {
+  return h > 9 ? `${h}:00` : `0${h}:00`;
+}
+
+function waterneed(x) {
+  return `${Math.round(x * 100)}%`;
+}
+
+function TableRow(x) {
+  return (
+    <div className={styles.tablerow} key={x.date}>
+      <div className={styles.item}>{`${x.date.getDate()} ${
+        months[x.date.getMonth()]
+      }`}</div>
+      <div className={styles.item}>{`${militarytimeFromHour(
+        x.date.getHours()
+      )}`}</div>
+      <div className={styles.item}>{`${waterneed(x.waterneed)}`}</div>
+    </div>
+  );
+}
+
+function TableHead() {
+  return (
+    <div className={styles.tablerow}>
+      <div className={styles.item}>Date</div>
+      <div className={styles.item}>Time</div>
+      <div className={styles.item}>Waterneed</div>
+    </div>
+  );
+}
+
+export default function Table({ weather }) {
+  if (!weather) {
+    return null;
+  }
+
   return (
     <div className={styles.table}>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+      {TableHead()}
+      {weather.map((x) => TableRow(x))}
     </div>
   );
 }
