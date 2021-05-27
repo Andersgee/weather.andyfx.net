43d855b9d4753e03ca2f3256b7ef8a5ef01a322a
andy <andersgee@gmail.com>
2021-05-27 16:03:03 +0200

    useWeather in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,23 @@
 import * as React from "react";
 import * as styles from "./main.module.scss";
+import useWeather from "@hooks/useWeather";
+
 import Nav from "@components/Nav";
 import Table from "@components/Table";
 import Visual from "@components/Visual";
 
 export default function Main() {
   const [active, setActive] = React.useState("table");
+
+  const lon = 16.33; //salbohed
+  const lat = 59.91;
+  const weather = useWeather(lon, lat);
+  console.log(weather);
+
   return (
     <div className={styles.main}>
       <Nav active={active} setActive={setActive} />
-      {active === "table" && <Table />}
+      {active === "table" && <Table weather={weather} />}
       {active === "visual" && <Visual />}
     </div>
   );
--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -1,4 +1,38 @@
 import { useState, useEffect } from "react";
+import { densitydifference, dewpoint } from "@js/stuff";
+
+function weatherlist(data) {
+  const w = data.list.map((x) => {
+    const X = {
+      date: new Date(x.dt_txt),
+      visibility: x.visibility, //Average visibility [m]
+      cloudiness: x.clouds.all / 100, //[0..100]
+      pop: x.pop, //probablility of precipitation [0..1] aka likelihood of rain or snow
+      rain: x.rain ? x.rain["3h"] / 3 : 0.0, // [mm/h]
+      snow: x.snow ? x.snow["3h"] / 3 : 0.0, // [mm/h]
+
+      temp: x.main.temp - 273.15, //temperature [Kelvin]
+      humidity: x.main.humidity / 100, //relative humidity [0..100]
+      pressure: x.main.grnd_level, //Atmospheric pressure on the ground [hPa]
+      pressure_sealevel: x.main.sea_level, //Atmospheric pressure on the sea level,
+
+      windspeed: x.wind.speed, //Wind speed. [m/s]
+      winddeg: x.wind.deg, //Wind direction, [degrees] (meteorological, aka where win is coming from, with north=0 and east=90)
+    };
+
+    //derived variables
+
+    //0 humidity at 30 C would give a value of 30 [g/m3] difference (basically max of what I could ever expect, so scale by this to get some 0..1 range)
+    const waterneed = densitydifference(X.temp, X.humidity) / 30;
+    const dewtemp = dewpoint(X.temp, X.humidity);
+
+    X.waterneed = waterneed;
+    X.dewtemp = dewtemp;
+
+    return X;
+  });
+  return w;
+}
 
 async function fetchweather(lon, lat) {
   const appid = "e95b492d3c977db6b58b95f097683036";
@@ -20,7 +54,7 @@ export default function useWeather(lon, lat) {
   useEffect(() => {
     fetchweather(lon, lat).then((w) => {
       if (w?.cod === "200") {
-        setWeather(w);
+        setWeather(weatherlist(w));
       }
     });
   }, [lon, lat]);
