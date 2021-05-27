8f46a923eca28359b8bfccca57f338bf34e43529
andy <andersgee@gmail.com>
2021-05-27 17:37:18 +0200

    table x scroll

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -24,6 +24,26 @@ function waterneed(x) {
   return `${Math.round(x * 100)}%`;
 }
 
+function temp(x) {
+  return `${Math.round(x)}°C`;
+}
+
+function humidity(x) {
+  return `${Math.round(x * 100)}%`;
+}
+
+function cloudiness(x) {
+  return `${Math.round(x * 100)}%`;
+}
+
+function rain(r, pop) {
+  return r > 0 ? `${Math.round(r * 100) / 100} mm/h` : "";
+}
+
+function windspeed(x) {
+  return `${Math.round(x)} m/s`;
+}
+
 function TableRow(x) {
   return (
     <div className={styles.tablerow} key={x.date}>
@@ -34,6 +54,11 @@ function TableRow(x) {
         x.date.getHours()
       )}`}</div>
       <div className={styles.item}>{`${waterneed(x.waterneed)}`}</div>
+      <div className={styles.item}>{`${temp(x.temp)}`}</div>
+      <div className={styles.item}>{`${humidity(x.humidity)}`}</div>
+      <div className={styles.item}>{`${cloudiness(x.cloudiness)}`}</div>
+      <div className={styles.item}>{`${rain(x.rain)}`}</div>
+      <div className={styles.item}>{`${windspeed(x.windspeed)}`}</div>
     </div>
   );
 }
@@ -44,6 +69,11 @@ function TableHead() {
       <div className={styles.item}>Date</div>
       <div className={styles.item}>Time</div>
       <div className={styles.item}>Waterneed</div>
+      <div className={styles.item}>Temperature</div>
+      <div className={styles.item}>Humidity</div>
+      <div className={styles.item}>Cloudiness</div>
+      <div className={styles.item}>Rain</div>
+      <div className={styles.item}>Windspeed</div>
     </div>
   );
 }
@@ -53,9 +83,11 @@ export default function Table({ weather }) {
     return null;
   }
 
+  const H = TableHead();
+
   return (
     <div className={styles.table}>
-      {TableHead()}
+      {H}
       {weather.map((x) => TableRow(x))}
     </div>
   );
