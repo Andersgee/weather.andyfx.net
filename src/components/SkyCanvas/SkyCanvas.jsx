76de277cd7337f40dd3f1ff67fb74ae542539dc8
Anders Gustafsson <andersgee@gmail.com>
2021-08-01 19:08:02 +0200

    canvas is showing clouds, no setUniforms yet

--- src/components/SkyCanvas/SkyCanvas.jsx
+++ src/components/SkyCanvas/SkyCanvas.jsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import { useRef, useEffect } from "react";
+import Skyweather from "@js/skyweather-gl";
 
-export default function SkyCanvas() {
-  const canvasref = React.useRef();
+export default function SkyCanvas({ glsl, textures }) {
+  const canvasref = useRef();
+
+  useEffect(() => {
+    const canvas = canvasref.current;
+    const sky = new Skyweather(canvas, glsl, textures);
+  }, []);
   return (
     <div>
       canvas below here
